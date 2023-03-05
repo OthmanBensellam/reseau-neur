d0d9a13d82c47e9e33e89174654054063f05efc8
OthmanBensellam <126090567+OthmanBensellam@users.noreply.github.com>
2023-03-05 08:48:13 +0100

    Update StandardLayer.cs

--- NeuralNetworks/NeuralNetwork/Layers/StandardLayer.cs
+++ NeuralNetworks/NeuralNetwork/Layers/StandardLayer.cs
@@ -1,4 +1,4 @@
-﻿using NeuralNetwork.Common.Activators;
+using NeuralNetwork.Common.Activators;
 using NeuralNetwork.Common.Layers;
 using NeuralNetwork.Common.GradientAdjustmentParameters;
 using NeuralNetwork.Common.GradientAdjustmentParameters;
@@ -37,7 +37,7 @@ namespace NeuralNetwork.Layers
         public Matrix<double> VelocityB_ { get; set; }
         public Matrix<double> VelocityW_ { get; set; }
 
-        public StandardLayer(Matrix<double> initialWeights, Matrix<double> initialBias, Matrix<double> VelocityB, Matrix<double> VelocityW, int batchSize, IActivator activator)
+        public StandardLayer(Matrix<double> initialWeights, Matrix<double> initialBias, int batchSize, IActivator activator)// Matrix<double> VelocityB, Matrix<double> VelocityW
         {
             LayerSize = initialWeights.ColumnCount;
             InputSize = initialWeights.RowCount;
@@ -46,19 +46,83 @@ namespace NeuralNetwork.Layers
             Bias_ = initialBias;
             Weights_ = initialWeights;
             //penser a trouver une architecture pr enlever la velocité d'ici
-            VelocityB_ = VelocityB;
-            VelocityW_ = VelocityW;
+            //VelocityB_ = VelocityB;
+            //VelocityW_ = VelocityW;
             Activation = Matrix<double>.Build.Dense(LayerSize, InputSize);
             //verify if need to be initialised (size etc)
-            GradWeight_ = Matrix<double>.Build.Dense(LayerSize, InputSize); ;
-            GradBias_ = Matrix<double>.Build.Dense(LayerSize, InputSize); ;
+            //GradWeight_ = Matrix<double>.Build.Dense(LayerSize, InputSize); ;
+            //GradBias_ = Matrix<double> ;
 
         }
 
         public void Propagate(Matrix<double> input )
         {
             int C_ = input.ColumnCount;
-            int NBatch = (int)(C_ / BatchSize);
+            int R_ = input.RowCount;
+            Matrix<double> M = Weights_.Transpose() * input + Bias_;
+            M.Map(Activator.Apply).CopyTo(Activation);
+        }
+
+
+        public void BackPropagate(Matrix<double> upstreamWeightedErrors)
+        {
+            int C_ = Activation.ColumnCount;
+            int R_ = Activation.RowCount;
+            Matrix<double> B = Activation.Map(Activator.ApplyDerivative) * upstreamWeightedErrors.Transpose();
+            WeightedError = (Weights_ * B).Transpose();
+            GradWeight_ = (Activation * B.Transpose()) / (double)BatchSize;
+            //creating a vector of BS ones
+            MathNet.Numerics.LinearAlgebra.Vector<double> v = MathNet.Numerics.LinearAlgebra.Vector<double>.Build.Dense(BatchSize, 1.0);
+            Matrix<double> VectorAsMatriceC = Matrix<double>.Build.Dense(BatchSize, 1);
+            VectorAsMatriceC.InsertColumn(0, v);
+            GradBias_ = (B* VectorAsMatriceC) / (double)BatchSize;
+        }
+        //C:\Users\othman\Downloads\dl-pricer\ensimag-dl-pricing\Examples\BooleanFunctionTester\xor-trained.json
+        public void UpdateParameters()
+        {
+            Weights_ -= (((FixedLearningRateParameters)Params_).LearningRate) * GradWeight_;
+            Bias_ -= (((FixedLearningRateParameters)Params_).LearningRate) * GradBias_;
+        }
+            //a priori le seul endroit qu'on aura a modifier est l'update parameter
+            //UP question a poser
+            /*
+            else if (Type_ == GradientAdjustmentType.Momentum)
+            {
+                while (i + NBatch < C_)
+                {
+                    //calcul de velocity peut etre ecrit d'une merilleure maniére:
+                    VelocityB_.SetSubMatrix(0, VelocityB_.RowCount, i, i + NBatch, ((MomentumParameters)Params_).Momentum * VelocityB_.SubMatrix(0, VelocityB_.RowCount, i, i + NBatch) - ((MomentumParameters)Params_).LearningRate * GradBias_.SubMatrix(0, GradBias_.RowCount, i, i + NBatch));
+                    VelocityW_.SetSubMatrix(0, VelocityW_.RowCount, i, i + NBatch, ((MomentumParameters)Params_).Momentum * VelocityW_.SubMatrix(0, VelocityW_.RowCount, i, i + NBatch) - ((MomentumParameters)Params_).LearningRate * GradWeight_.SubMatrix(0, GradWeight_.RowCount, i, i + NBatch));
+                    Weights_.SetSubMatrix(0, Weights_.RowCount, i, i + NBatch, Weights_.SubMatrix(0, Weights_.RowCount, i, i + NBatch) + VelocityW_.SubMatrix(0, VelocityW_.RowCount, i, i + NBatch));
+                    Bias_.SetSubMatrix(0, Bias_.RowCount, i, i + NBatch, Bias_.SubMatrix(0, Bias_.RowCount, i, i + NBatch) + VelocityB_.SubMatrix(0, VelocityB_.RowCount, i, i + NBatch));
+                    i += NBatch;
+                }
+                if (i < C_ - 1)
+                {
+                    NBatch = C_ - 1 - i;
+                    //calcul de velocity peut etre ecrit d'une merilleure maniére:
+                    VelocityB_.SetSubMatrix(0, VelocityB_.RowCount, i, i + NBatch, ((MomentumParameters)Params_).Momentum * VelocityB_.SubMatrix(0, VelocityB_.RowCount, i, i + NBatch) - ((MomentumParameters)Params_).LearningRate * GradBias_.SubMatrix(0, GradBias_.RowCount, i, i + NBatch));
+                    VelocityW_.SetSubMatrix(0, VelocityW_.RowCount, i, i + NBatch, ((MomentumParameters)Params_).Momentum * VelocityW_.SubMatrix(0, VelocityW_.RowCount, i, i + NBatch) - ((MomentumParameters)Params_).LearningRate * GradWeight_.SubMatrix(0, GradWeight_.RowCount, i, i + NBatch));
+                    Weights_.SetSubMatrix(0, Weights_.RowCount, i, i + NBatch, Weights_.SubMatrix(0, Weights_.RowCount, i, i + NBatch) + VelocityW_.SubMatrix(0, VelocityW_.RowCount, i, i + NBatch) );
+                    Bias_.SetSubMatrix(0, Bias_.RowCount, i, i + NBatch, Bias_.SubMatrix(0, Bias_.RowCount, i, i + NBatch) + VelocityB_.SubMatrix(0, VelocityB_.RowCount, i, i + NBatch));
+                }
+            }*/
+
+        public bool Equals(ILayer other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
+
+
+
+
+
+//the old version of the props for the batching:
+/*
+ * int NBatch = (int)(C_ / BatchSize);
             int i = 0;
             while(i+NBatch<C_)
             {
@@ -73,12 +137,8 @@ namespace NeuralNetwork.Layers
                 Matrix<double> M = Weights_.SubMatrix(0, Weights_.RowCount, i, i + NBatch).Transpose() + Bias_.SubMatrix(0, Bias_.RowCount, i, i + NBatch);
                 Activation.SetSubMatrix(0, Bias_.RowCount, i, i + NBatch, M.Map(Activator.Apply));
             }
-        }
+Back prop
 
-
-        public void BackPropagate(Matrix<double> upstreamWeightedErrors)
-        {
-            int C_ = Activation.ColumnCount;
             int NBatch = (int)(C_ / BatchSize);
             int i = 0;
             MathNet.Numerics.LinearAlgebra.Vector<double> v = MathNet.Numerics.LinearAlgebra.Vector<double>.Build.Dense(NBatch, 1.0);
@@ -100,57 +160,20 @@ namespace NeuralNetwork.Layers
                 GradWeight_.SetSubMatrix(0, GradWeight_.RowCount, i, i + NBatch, (Activation.SubMatrix(0, Activation.RowCount, i, i + NBatch) * B.Transpose()) / (double)BatchSize);
                 GradBias_.SetSubMatrix(0, GradBias_.RowCount, i, i + NBatch, B * (double)BatchSize);
             }
-        }
-        //C:\Users\othman\Downloads\dl-pricer\ensimag-dl-pricing\Examples\BooleanFunctionTester\xor-trained.json
-        public void UpdateParameters()
-        {//sc2
-         // sah re reflechir je suis perdu un peu 
-            int C_ = Activation.ColumnCount;
+ int C_ = Activation.ColumnCount;
             int NBatch = (int)(C_ / BatchSize);
             int i = 0;
             if (Type_ == GradientAdjustmentType.FixedLearningRate)
             {
                 while (i + NBatch < C_)
                 {
-                    Weights_.SetSubMatrix(0, Weights_.RowCount, i, i + NBatch, Weights_.SubMatrix(0, Weights_.RowCount, i, i + NBatch) - ((FixedLearningRateParameters)Params_).LearningRate * GradWeight_.SubMatrix(0, GradWeight_.RowCount, i, i + NBatch));
-                    Bias_.SetSubMatrix(0, Bias_.RowCount, i, i + NBatch, Bias_.SubMatrix(0, Bias_.RowCount, i, i + NBatch) - ((FixedLearningRateParameters)Params_).LearningRate * GradBias_.SubMatrix(0, GradBias_.RowCount, i, i + NBatch));
-                    i += NBatch;
-                }
-                if (i < C_ - 1)
-                {
-                    NBatch = C_ - 1 - i;
-                    Weights_.SetSubMatrix(0, Weights_.RowCount, i, i + NBatch, Weights_.SubMatrix(0, Weights_.RowCount, i, i + NBatch) - ((FixedLearningRateParameters)Params_).LearningRate * GradWeight_.SubMatrix(0, GradWeight_.RowCount, i, i + NBatch));
-                    Bias_.SetSubMatrix(0, Bias_.RowCount, i, i + NBatch, Bias_.SubMatrix(0, Bias_.RowCount, i, i + NBatch) - ((FixedLearningRateParameters)Params_).LearningRate * GradBias_.SubMatrix(0, GradBias_.RowCount, i, i + NBatch));
-                }
-            }
-            //a priori le seul endroit qu'on aura a modifier est l'update parameter
-            //UP question a poser 
-            else if (Type_ == GradientAdjustmentType.Momentum)
-            {
-                while (i + NBatch < C_)
-                {
-                    //calcul de velocity peut etre ecrit d'une merilleure maniére:
-                    VelocityB_.SetSubMatrix(0, VelocityB_.RowCount, i, i + NBatch, ((MomentumParameters)Params_).Momentum * VelocityB_.SubMatrix(0, VelocityB_.RowCount, i, i + NBatch) - ((MomentumParameters)Params_).LearningRate * GradBias_.SubMatrix(0, GradBias_.RowCount, i, i + NBatch));
-                    VelocityW_.SetSubMatrix(0, VelocityW_.RowCount, i, i + NBatch, ((MomentumParameters)Params_).Momentum * VelocityW_.SubMatrix(0, VelocityW_.RowCount, i, i + NBatch) - ((MomentumParameters)Params_).LearningRate * GradWeight_.SubMatrix(0, GradWeight_.RowCount, i, i + NBatch));
-                    Weights_.SetSubMatrix(0, Weights_.RowCount, i, i + NBatch, Weights_.SubMatrix(0, Weights_.RowCount, i, i + NBatch) + VelocityW_.SubMatrix(0, VelocityW_.RowCount, i, i + NBatch));
-                    Bias_.SetSubMatrix(0, Bias_.RowCount, i, i + NBatch, Bias_.SubMatrix(0, Bias_.RowCount, i, i + NBatch) + VelocityB_.SubMatrix(0, VelocityB_.RowCount, i, i + NBatch));
-                    i += NBatch;
-                }
-                if (i < C_ - 1)
-                {
-                    NBatch = C_ - 1 - i;
-                    //calcul de velocity peut etre ecrit d'une merilleure maniére:
-                    VelocityB_.SetSubMatrix(0, VelocityB_.RowCount, i, i + NBatch, ((MomentumParameters)Params_).Momentum * VelocityB_.SubMatrix(0, VelocityB_.RowCount, i, i + NBatch) - ((MomentumParameters)Params_).LearningRate * GradBias_.SubMatrix(0, GradBias_.RowCount, i, i + NBatch));
-                    VelocityW_.SetSubMatrix(0, VelocityW_.RowCount, i, i + NBatch, ((MomentumParameters)Params_).Momentum * VelocityW_.SubMatrix(0, VelocityW_.RowCount, i, i + NBatch) - ((MomentumParameters)Params_).LearningRate * GradWeight_.SubMatrix(0, GradWeight_.RowCount, i, i + NBatch));
-                    Weights_.SetSubMatrix(0, Weights_.RowCount, i, i + NBatch, Weights_.SubMatrix(0, Weights_.RowCount, i, i + NBatch) + VelocityW_.SubMatrix(0, VelocityW_.RowCount, i, i + NBatch) );
-                    Bias_.SetSubMatrix(0, Bias_.RowCount, i, i + NBatch, Bias_.SubMatrix(0, Bias_.RowCount, i, i + NBatch) + VelocityB_.SubMatrix(0, VelocityB_.RowCount, i, i + NBatch));
-                }
-            }
-        }
 
-        public bool Equals(ILayer other)
-        {
-            throw new NotImplementedException();
-        }
-    }
-}
+                    i += NBatch;
+                }
+                if (i < C_ - 1)
+                {
+                    NBatch = C_ - 1 - i;
+                    Weights_.SetSubMatrix(0, Weights_.RowCount, i, i + NBatch, Weights_.SubMatrix(0, Weights_.RowCount, i, i + NBatch) - ((FixedLearningRateParameters)Params_).LearningRate * GradWeight_.SubMatrix(0, GradWeight_.RowCount, i, i + NBatch));
+                    Bias_.SetSubMatrix(0, Bias_.RowCount, i, i + NBatch, Bias_.SubMatrix(0, Bias_.RowCount, i, i + NBatch) - ((FixedLearningRateParameters)Params_).LearningRate * GradBias_.SubMatrix(0, GradBias_.RowCount, i, i + NBatch));
+                }
+*/
